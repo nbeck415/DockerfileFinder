c077b80f231a9635e01047d0fb3c9bde4a59007f
Natalie Lunbeck <natalielunbeck@yahoo.com>
2023-10-24 13:29:54 -0700

    add more search params

--- dockerfile_finder.py
+++ dockerfile_finder.py
@@ -1,6 +1,13 @@
 import requests, os
 from dotenv import load_dotenv
 
+n_stars = 100
+pages = 10
+sort = 'stars'
+order = 'desc'
+per_page = 100
+target_filenames = ["Dockerfile", "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml"]
+
 def setup():
     load_dotenv()
     token = os.getenv('GH_ACCESS')
@@ -11,7 +18,7 @@ def setup():
 
 def find_dockerfile(auth):
     repo_addrs = []
-    url = 'https://api.github.com/search/repositories?q=sort=stars&order=desc&per_page=100'
+    url = f'https://api.github.com/search/repositories?q=topic:hacktoberfest&sort={sort}&order={order}&per_page={per_page}&stars:>{n_stars}'
     response = requests.get(url, headers=auth)
     if response.status_code == 200:
         data = response.json()
@@ -20,11 +27,16 @@ def find_dockerfile(auth):
             name = repo['name']
             owner = repo['owner']['login']
             repo_url = f'https://api.github.com/repos/{owner}/{name}/contents'
-            contents = requests.get(repo_url, headers=auth)
+            headers = {'Authorization': auth['Authorization'], 'Accept': 'application/vnd.github.v3.raw'}
+            contents = requests.get(repo_url, headers=headers)
             repo_contents = contents.json()
             for item in repo_contents:
-                if item['name'] == 'Dockerfile':
-                    repo_addrs.append((owner, name))
+                if item['name'] in target_filenames:
+                    found_file = item['name']
+                    repo_addr = (owner, name)
+                    if repo_addr not in repo_addrs:
+                        repo_addrs.append(repo_addr)
+                    print(f'{found_file} found in {owner}/{name}')
     else:
         print(f'Status code: {response.status_code}')
     return repo_addrs
