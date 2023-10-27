6cb9b7b9062573b3ddab85ead65952ee8eb5eaa9
Natalie <natalielunbeck@yahoo.com>
2023-10-27 11:33:39 -0700

    add search results function

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import dockerfile_finder as dff
 
 app = Flask(__name__)
+auth = dff.setup()
 
 @app.route('/')
 def index():
     topics = dff.get_topics()
     return render_template('index.html', topics=topics)
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        print(request.form.get('num-stars'))
+        print(request.form.get('topics'))
+        repos = dff.find_dockerfile(auth, stars=request.form.get('num-stars'), topic=request.form.get('topics'))
+        return render_template('results.html', repos=repos)
+    return render_template('results.html', repos=repos)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
